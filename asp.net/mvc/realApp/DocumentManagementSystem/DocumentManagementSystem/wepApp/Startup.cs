05c22f87f4ac1c306f7ea4e34948473c9dd39042
xautopilot <xautopilot@users.noreply.github.com>
2015-12-08 15:02:27 +0600

    document management system initial commit with all basic package needed

---
+++ asp.net/mvc/realApp/DocumentManagementSystem/DocumentManagementSystem/wepApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(wepApp.Startup))]
+namespace wepApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
