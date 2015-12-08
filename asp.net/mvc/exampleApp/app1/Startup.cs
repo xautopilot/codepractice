31000beca5b4ff12dae0022f686e1ef90b7d5c0c
xautopilot <xautopilot@users.noreply.github.com>
2015-12-08 13:17:28 +0600

    mvc example app initial commit

---
+++ asp.net/mvc/exampleApp/app1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(app1.Startup))]
+namespace app1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
